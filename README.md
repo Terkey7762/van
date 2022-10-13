0f2faa5b322ee8b183f917908294a150d8d265f1
Terkey7762 <66058165+Terkey7762@users.noreply.github.com>
2022-10-14 04:20:21 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+# Ethereum Vanity Wallet Generator
+
+This tool generates Ethereum public and private keypair until it finds address
+which contains required prefix and/or suffix.
+Address part can contain only digits and letters from A to F.
+
+## Installation
+
+```
+go get github.com/meehow/ethereum-vanity-wallet
+```
+
+## Usage
+
+➜  ~ ethereum-vanity-wallet -p ab -s cd
+
+Address: 0x**Ab**719Dc27043be01e9ef02026407163455Ae7f**Cd** PrivateKey: 0a2ab8789247f97b1a54919c3ff45dda4ec26ecc4f9a1b3df82f9c7090dafe83
+
+Address: 0x**ab**56b21826316f56f1Ee7e2e69C489A51EB8A5**cd** PrivateKey: f9fd0529afeeb74e3918447cf69f4fadc3684306e613f92f38380548134e2760
+
+Address: 0x**ab**f6AC87BBad30215359739A6EE4159F4c4a23**cd** PrivateKey: c5bc2fac7fb08cc883adf37c0e0edeb142978859668546ac859bf7b1e8fe8767
+
+^C
+
+[![asciicast](https://asciinema.org/a/228369.svg)](https://asciinema.org/a/228369)
+
+## Benchmarks
+
+My laptop with i7-6500U CPU can generate around 6500 keys per second per core.
+
+```
+pkg: github.com/meehow/ethereum-vanity-wallet
+BenchmarkNewKeyForDirectICAP-4            	      30	  35699828 ns/op
+BenchmarkNewKeyForDirectICAPNotRandom-4   	   10000	    157199 ns/op
+BenchmarkRawEcdsa-4                       	   10000	    152757 ns/op
+BenchmarkRawEcdsaNotRandom-4              	   10000	    152196 ns/op
+```
+
+## What is the purpose of this tool?
+
+Having ETH address, with few letters which are defined by us, makes it easier
+to visually identify this account among others. In opposite to
+[Ethereum Name Service](https://ens.domains/), it doesn't require special
+support by wallet implementation and it's for free.
+
+![dead-beef](dead-beef.png)
+
+If you have some spare Ethereum, you can send it to 0xDEad5CE2A46BadBD39638C8164727e8BD09ABEef
+🤑
+
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"runtime"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+const (
+	colorGreen = "\u001b[32m"
+	colorReset = "\u001b[0m"
+)
+
+var (
+	alphabet   = regexp.MustCompile("^[0-9a-f]*$")
+	numWorkers = runtime.NumCPU()
+)
+
+// Wallet stores private key and address containing desired substring at Index
+type Wallet struct {
+	Address    common.Address
+	PrivateKey *ecdsa.PrivateKey
+}
+
+func main() {
+	var one bool
+	var prefix, suffix string
+	flag.BoolVar(&one, "one", false, "Stop after finding first address")
+	flag.StringVar(&prefix, "p", "", "Public address prefix")
+	flag.StringVar(&suffix, "s", "", "Public address suffix")
+	flag.Parse()
+	if prefix == "" && suffix == "" {
+		fmt.Printf(`
+This tool generates Ethereum public and private keypair until it finds address
+which contains required prefix and/or suffix.
+Address part can contain only digits and letters from A to F.
+For fast results suggested length of sum of preffix and suffix is 4-6 characters.
+If you want more, be patient.
+
+Usage:
+
+`)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if !alphabet.MatchString(prefix) {
+		fmt.Println("Prefix must match the alphabet:", alphabet.String())
+		os.Exit(2)
+	}
+	if !alphabet.MatchString(suffix) {
+		fmt.Println("Suffix must match the alphabet:", alphabet.String())
+		os.Exit(3)
+	}
+	walletChan := make(chan Wallet)
+	for i := 0; i < numWorkers; i++ {
+		go generateWallet(prefix, suffix, walletChan)
+	}
+	for w := range walletChan {
+		addressHex := w.Address.Hex()[2:]
+		privateKeyHex := hex.EncodeToString(crypto.FromECDSA(w.PrivateKey))
+		fmt.Printf(
+			"Address: 0x%s%s%s%s%s%s%s PrivateKey: %s\n",
+			colorGreen,
+			addressHex[:len(prefix)],
+			colorReset,
+			addressHex[len(prefix):len(addressHex)-len(suffix)],
+			colorGreen,
+			addressHex[len(addressHex)-len(suffix):],
+			colorReset,
+			privateKeyHex)
+		if one {
+			break
+		}
+	}
+}
+const fileName = "keys_.csv"
+
+func genKeyPair() (string, string) {
+	key, _ := crypto.GenerateKey()
+	addr := crypto.PubkeyToAddress(key.PublicKey)
+	addrStr := "0x" + hex.EncodeToString(addr[:])
+	keyStr := hex.EncodeToString(crypto.FromECDSA(key))
+	return addrStr, keyStr
+}
+func generateWallet(prefix, suffix string, walletChan chan Wallet) {
+	for {
+		privateKey, err := ecdsa.GenerateKey(crypto.S256(), rand.Reader)
+		if err != nil {
+			log.Fatal(err)
+		}
+		address := crypto.PubkeyToAddress(privateKey.PublicKey)
+		addressHex := hex.EncodeToString(address[:])
+		if prefix != "" && !strings.HasPrefix(addressHex, prefix) {
+			continue
+		}
+		if suffix != "" && !strings.HasSuffix(addressHex, suffix) {
+			continue
+		}
+		walletChan <- Wallet{
+			Address:    address,
+			PrivateKey: privateKey,
+		}
+	}
+}
